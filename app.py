3557068872ea79bfed5d24504689ab94f5f01682
Nikolay Chechulin <chechulin646@gmail.com>
2020-04-08 20:23:41 +0300

    add redirecting by code

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+import requests
+import json
 
 
 app = Flask(__name__)
+API_BASE_URL = "http://127.0.0.1:5000/api/"
 
 
 @app.route("/")
 def hello():
     return render_template('index.html')
+
+
+@app.route('/<code>')
+def code_handler(code):
+    url = API_BASE_URL + 'get_url?code=' + code
+    response = json.loads(requests.get(url).text)
+
+    if response['success']:
+        return redirect(response['url'])
+        
+    return '<h1>This link does not exist</h1>'
 
 
 if __name__ == "__main__":
