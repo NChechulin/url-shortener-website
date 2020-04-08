f0ee99f9782a443671ec31e98ecae162a086a3c4
Nikolay Chechulin <chechulin646@gmail.com>
2020-04-08 19:46:05 +0300

    change dummy request to correct one

--- api.js
+++ api.js
@@ -1,24 +1,33 @@
-var base_url = "http://127.0.0.1:5000/api/"
+var api_base_url = "http://127.0.0.1:5000/api/"
+var website_base_url = "http://127.0.0.1:8000/"
 
 function display_response(response) {
     if (response['success']) {
         var code = response['code'];
 
-        document.getElementById("result_url").value = code;
+        document.getElementById("result_url").value = website_base_url + code;
         document.getElementById('result').style.visibility = 'visible';
     } else {
         alert('error: ' + response['message']);
     }
 }
 
+function get_url_to_shorten() {
+    var url_input = document.getElementById('full_url_input');
+    return url_input.value;
+}
+
 function shorten_url() {
     var xhttp = new XMLHttpRequest();
+    var url = get_url_to_shorten();
+
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             var response = JSON.parse(this.response)
             display_response(response);
         }
     };
-    xhttp.open("GET", "http://127.0.0.1:5000/api/shorten_link?url=https://www.youtube.com/watch?v=Q4_42Zm-DrI", true);
+
+    xhttp.open("GET", api_base_url + 'shorten_link?url=' + url, true);
     xhttp.send();
 }
